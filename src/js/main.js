3a90cc680cef6d9aef3560d342f25eae433113be
Marina <84480963+Mariannadtd@users.noreply.github.com>
2022-09-14 20:46:27 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-import modals from './modules/modals';
+import modals from './modules/modals.js';
+import forms from './modules/forms.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
   modals();
+  forms();
 });
